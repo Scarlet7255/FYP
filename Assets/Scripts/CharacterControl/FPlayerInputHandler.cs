8d93f8b18aad20dd3813ff92c933de2bd43f4fc3
unknown <1754283508@qq.com>
2023-03-15 22:17:07 +0800

    First person view to god view

--- Assets/Scripts/CharacterControl/FPlayerInputHandler.cs
+++ Assets/Scripts/CharacterControl/FPlayerInputHandler.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(MovementControl))]
-public class PlayerInputHandler : MonoBehaviour
+public class FPlayerInputHandler : MonoBehaviour
 {
     public float mouseSensitivity = 1.0f;
     
     // camera control properity
-    public Camera viewCamera;
-    public float _cameraAngleX = 0.0f;
-    public float cameraAngleML = 15.1f;
-    public float cameraAngleLL = -20.1f;
     private MovementControl _controller;
 
     private void Start()
@@ -26,13 +22,6 @@ public class PlayerInputHandler : MonoBehaviour
     {
         Vector2 angle = callbackContext.ReadValue<Vector2>();
         _controller.Rotate(angle.x*mouseSensitivity);
-        
-        float deltaAngle = angle.y * mouseSensitivity * -1.0f;
-        if(_cameraAngleX + deltaAngle<cameraAngleML&&_cameraAngleX + deltaAngle>cameraAngleLL)
-        {
-            viewCamera.transform.Rotate(angle.y * mouseSensitivity * -1.0f, 0.0f, 0.0f);
-            _cameraAngleX += deltaAngle;
-        }
     }
 
     public void Move(InputAction.CallbackContext callbackContext)
